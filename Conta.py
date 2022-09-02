dbb82b3ccccbbce74ab1da5661a64c5b1be880be
Lucas Leão <saraivaleaolucas@gmail.com>
2022-09-02 01:44:39 -0300

    realizado o processo de saque, deposito e extrato, explorando a POO

--- Conta.py
+++ Conta.py
@@ -14,3 +14,15 @@ class Conta:
             print('O saldo não pode ser negativo')
         else:
             self._saldo = saldo
+
+    def saque(self, valor):
+        if (self.saldo >= valor):
+            self.saldo -= valor
+        else:
+            print('saldo insuficiente')
+
+    def deposita(self, valor):
+        self.saldo += valor
+    
+    def extrato(self):
+        print("cliente: ", self._titular, " Saldo Atual", self._saldo)
--- Main.py
+++ Main.py
@@ -9,10 +9,17 @@ class Main:
 print('Testando o projeto')
 
 
-c1 = Cliente('João', '144-222')
-conta = Conta(c1._nome, 6565, 0)
+# c1 = Cliente('João', '144-222')
+# conta = Conta(c1._nome, 6565, 0)
 
-print(conta._titular, "Número: ", conta._numero, "Seu Saldo: ", conta._saldo)
+# print(conta._titular, "Número: ", conta._numero, "Seu Saldo: ", conta._saldo)
 
 # print(c1)
 # print(c1.nome, " e ", c1.telefone)
+
+c1 = Cliente("João", "114444-2222")
+conta = Conta(c1.get_nome(), 1222, 0)
+
+conta.deposita(100)
+conta.saque(50)
+conta.extrato()
